6073b82c80f592614d4de0910c38a43a949c9e5b
Eyrie <eyriechen@gmail.com>
2014-02-18 22:01:18 +0800

    big bang

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HzydjsErp::Application.config.secret_key_base = '23023069678dcfd64c506e7805636dbb59811d7bd6c5d95c99d0ad510752d6aeb6cba72b86e46c391b57c9102805a714ce2f8c0c6986a71d36c8afb0eeb8c202'
